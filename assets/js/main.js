7e3850159455b420e30bae9a20c41c98f7966598
Larissa <contatolarisn@gmail.com>
2023-04-19 20:24:26 -0300

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -17,7 +17,7 @@ function convertPokemonToLi(pokemon) {
                 </ol>
 
                 <img src="${pokemon.photo}"
-                     alt="${pokemon.name}">
+                alt="${pokemon.name}">
             </div>
         </li>
     `
